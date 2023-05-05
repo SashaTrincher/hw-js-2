293c81748e6e1604c7adff5f0c540350fec2f7b6
SashaTrincher <oofman@ukr.net>
2023-05-05 18:30:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let myNumber = 10;
+let myNumberTwo = 20;
+let myNumberThree = 30;
+let myNumberFour = 10.4;
+
+console.log(Number.parseInt(myNumber, myNumberTwo, myNumberThree));
+console.log(Number.parseFloat(myNumber, myNumberTwo, myNumberThree));
+
+console.log(Math.random(myNumber, myNumberTwo));
+console.log(Math.round(myNumberFour));
+console.log(Math.floor(myNumberFour));
+console.log(Math.ceil(myNumberFour));
+console.log(Math.max(myNumber, myNumberTwo));
+console.log(Math.min(myNumber, myNumberTwo));
+console.log(Math.pow(myNumber, myNumberTwo));
